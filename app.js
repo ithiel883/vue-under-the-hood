2a036c7cfe722c8a576b0bd569d7be9badbe365b
giranPaladin <ajuka.zephiniah@gmail.com>
2021-12-23 00:32:47 +0100

    main file

---
+++ app.js
@@ -0,0 +1,28 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      currentUserInput: '',
+      message: 'Vue is great!',
+    };
+  },
+  methods: {
+    saveInput(event) {
+      this.currentUserInput = event.target.value;
+    },
+    setText() {
+      // this.message = this.currentUserInput;
+      this.message = this.$refs.userText.value
+    },
+  },
+  beforeCreate() {
+    console.log('beforeCreate()')
+  },
+  created() {
+    console.log('created')
+  },
+});
+
+app.mount('#app');
+
+////
+
